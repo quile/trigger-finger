31a78b5f18443dd3b9435fb87404ddc27ebec1ab
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-04-17 14:00:20 -0700

    Basic dir watching

--- trigger-finger.js
+++ trigger-finger.js
@@ -4,20 +4,28 @@ var optimist = require("optimist")
                .default("dir", [ "." ])
                .argv;
 
-var fs = require("fs");
-var _  = require("underscore");
+var fs   = require("fs");
+var _    = require("underscore");
 var walk = require("walk");
+var exec = require("child_process").exec;
 
 console.log("Trigger-finger watching directories: " + optimist.dir);
 
 function handleChanges( dir, event, filename ) {
   console.log("Detected changes to " + filename + " in " + dir);
+  console.log("Executing [" + optimist.command + "]...");
+
+  exec( optimist.command, function( error, stdout, stderr ) {
+    if ( error ) {
+      console.error( stderr );
+    } else {
+      console.log( stdout );
+    }
+  });
 }
 
 function createWatchers(dir) {
-
   var walker = walk.walk( dir, { followLinks: false } );
-
   walker.on("directories", function( root, stats, next ) {
 
     stats.forEach( function( s ) {
@@ -30,6 +38,4 @@ function createWatchers(dir) {
   });
 }
 
-_( optimist.dir ).each(
-  createWatchers
-);
+_( optimist.dir ).each( createWatchers );
